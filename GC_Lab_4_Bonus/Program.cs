f150ce1146631faf53ecee5f7e197ff47c4f3801
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-05 10:22:18 -0500

    completed base requirements

--- GC_Lab_4_Bonus/Program.cs
+++ GC_Lab_4_Bonus/Program.cs
@@ -6,7 +6,73 @@ namespace GC_Lab_4_Bonus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                string userInput = GetLowerCaseStringFromUser("Enter in a word: ");
+
+                string pigLatin = PigLatanizeWord(userInput);
+
+                Console.WriteLine(pigLatin);
+                Console.WriteLine();
+
+            } while (PromptForLoop("Convert another word? (y/n) \n"));
+        }
+
+        private static string PigLatanizeWord(string word)
+        {
+            if (string.IsNullOrWhiteSpace(word)) return "";
+            string output = string.Empty;
+            string firstChar = word[0].ToString();
+
+            if ("AaEeIiOoUu".Contains(firstChar))
+            {
+                output += firstChar;
+            }
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                output += word[i];
+            }
+
+            if ("AaEeIiOoUu".Contains(firstChar))
+            {
+                output += "way";
+            }
+            else
+            {
+                output += firstChar + "ay";
+            }
+
+
+            return output;
+        }
+
+        private static string GetLowerCaseStringFromUser(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine().ToLower();
+        }
+
+        private static bool PromptForLoop(string prompt)
+        {
+            do
+            {
+                Console.Write(prompt);
+                char key = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if ("Yy".Contains(key))
+                {
+                    return true;
+                }
+                else if ("Nn".Contains(key))
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("type y for yes or n for no");
+                }
+            } while (true);
         }
     }
 }
